4e7c2739c121c5b784809ce0e8f145060e0e4ab2
eiji <eijimine@gmail.com>
2017-07-18 15:44:06 -0400

    Image App Complete:

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root 'pictures#index'
   get 'pictures' => 'pictures#index'
 
+  delete 'pictures/:id' => 'pictures#destroy'
+
+  get 'pictures/:id/edit' => "pictures#edit"
+ patch 'pictures/:id' => "pictures#update"
+
   post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
 
